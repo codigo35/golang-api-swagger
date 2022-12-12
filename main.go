f89db2094cacf02335eb3ae3d6a4131e12cee424
pedro.mendes <pedro.mendes@g.globo>
2022-12-12 14:32:38 -0300

    feat:  add statuscode handler

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type StatusCodeHandlerPayload struct {
+	Code int `json:"code"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", helloWorldHandler)
+	e.POST("/status", statusCodeHandler)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func helloWorldHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func statusCodeHandler(c echo.Context) error {
+	payload := new(StatusCodeHandlerPayload)
+	if err := c.Bind(payload); err != nil {
+		return err
+	}
+	return c.String(http.StatusOK, fmt.Sprintf("Status Code: %d", payload.Code))
 }
