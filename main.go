a6bf6517c858caa707ef67e668a0f83b2bd56802
pedro.mendes <pedro.mendes@g.globo>
2022-12-12 14:32:11 -0300

    feat: add helloworld handler

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", helloWorldHandler)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func helloWorldHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
